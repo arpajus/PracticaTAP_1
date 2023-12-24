96de53633c84cf9064236f6574f223ed78a26bac
Adrian Garcia <adrian.garcia@estudiants.urv.cat>
2023-12-24 15:29:51 +0100

    test done

--- src/test/MyTest.java
+++ src/test/MyTest.java
@@ -1,12 +1,26 @@
 package test;
 
+import main.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class MyTest {
+import org.junit.Test;
+
+public class MyTest {
+
+    Controller controller = new Controller();
+    RoundRobinImproved roundRobinImproved = new RoundRobinImproved();
 
     @Test
-    void addition() {
-        assertEquals(2,2,"");
-    }
+    public void addition() {
 
+        controller.setPolicy(roundRobinImproved);
+
+        Invoker iv1 = new Invoker(1000);
+        controller.addInvoker(iv1);
+        Invoker iv2 = new Invoker(2000);
+        controller.addInvoker(iv2);
+
+        assertEquals(iv1.getTotalMemory(), 1000, "iv1 ok" + iv1.toString());
+        assertEquals(iv2.getTotalMemory(), 2000, "iv2 ok" + iv2.toString());
+    }
 }
