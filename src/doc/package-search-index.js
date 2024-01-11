0896aa20b8dbb7473ea8a02d69b01c98dc009185
arpajus <arpajus@gmail.com>
2024-01-11 19:07:55 +0100

    JavaDoc

---
+++ src/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"main.decorator"},{"l":"main.exceptions"},{"l":"main.interfaces"},{"l":"main.main"},{"l":"main.mapReduce"},{"l":"main.operations"},{"l":"main.policy"},{"l":"main.reflection"}];updateSearchResults();
